d1d5d174bb519464a2849ff9e683e2ed65488a4b
junnelmacabuhay <jmacabuhay3@ssct.edu.ph>
2024-05-30 15:23:23 +0800

    Add files via upload
    Signed-off-by: junnelmacabuhay <jmacabuhay3@ssct.edu.ph>

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const sampleData = {
     labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     datasets: [{
-      label: 'MY DAILY DURING CLASS DAYS',
-      data: [50, 100, 50, 100, 50, 100, 50, 100, 50, 100, 70],
+      label: 'DAILY EXPENSES DURING CLASS HOURS',
+      data: [50, 100, 50, 100, 50, 100, 50, 100, 50, 100, 50, 80],
       backgroundColor: 'rgba(168, 56, 141, 0.8',
       borderColor: 'rgba(28, 255, 205, 0.91)',
       borderWidth: 1
-    }]S
+    }]
   };
   
   const ctx = document.getElementById('myChart').getContext('2d');
