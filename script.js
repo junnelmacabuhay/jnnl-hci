862153dbd987e8ab0f82121f8b406c940ad7d3dd
junnelmacabuhay <jmacabuhay3@ssct.edu.ph>
2024-05-30 14:54:39 +0800

    Update script.js
    Signed-off-by: junnelmacabuhay <jmacabuhay3@ssct.edu.ph>

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleData = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June'],
     datasets: [{
       label: 'MY EXPENSES MONTHLY',
-      data: [500, 600, 700, 790, 810, 899, 955, 900, 430, 200, 350, 999],
+      data: [1,500, 1,500, 1,500, 1,500, 1,500, 1,500, 1,500, 1,500, 1,500, 1,500, 1,500, 1,500],
       backgroundColor: 'rgba(168, 56, 141, 0.8',
       borderColor: 'rgba(28, 255, 205, 0.91)',
       borderWidth: 1
