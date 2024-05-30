e12407dd41a677b2aaa439f010a7082b83bf26ec
junnelmacabuhay <jmacabuhay3@ssct.edu.ph>
2024-05-30 15:11:04 +0800

    Update script.js
    Signed-off-by: junnelmacabuhay <jmacabuhay3@ssct.edu.ph>

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const sampleData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     datasets: [{
-      label: 'MY EXPENSES MONTHLY',
-      data: [1,000, 1,000, 1,000, 1,000, 1,000, 1,000, 1,000, 1,000, 1,000, 1,000, 1,000, 1,000],
+      label: 'MY DAILY DURING CLASS DAYS',
+      data: [50, 100, 50, 100, 50, 100, 50, 100, 50, 100, 70],
       backgroundColor: 'rgba(168, 56, 141, 0.8',
       borderColor: 'rgba(28, 255, 205, 0.91)',
       borderWidth: 1
-    }]
+    }]S
   };
   
   const ctx = document.getElementById('myChart').getContext('2d');
